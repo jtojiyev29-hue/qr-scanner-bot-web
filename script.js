e20542ccd0a7d84851c47aa1cc1086e4007f24bd
jtojiyev29-hue <jtojiyev29@gmail.com>
2025-09-27 17:40:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const resultElement = document.getElementById("result");
+let lastScanned = null;
+
+function startScanner() {
+  const html5QrCode = new Html5Qrcode("preview");
+  html5QrCode.start(
+    { facingMode: "environment" }, // orqa kamera
+    { fps: 10, qrbox: 250 },
+    (decodedText) => {
+      lastScanned = decodedText;
+      resultElement.textContent = decodedText;
+    },
+    (errorMessage) => {
+      // console.log("Skaner xatosi:", errorMessage);
+    }
+  ).catch(err => {
+    console.error("Kamerani ochib bo‘lmadi:", err);
+  });
+}
+
+startScanner();
+
+// Copy tugmasi
+document.getElementById("copyBtn").addEventListener("click", () => {
+  if (lastScanned) {
+    navigator.clipboard.writeText(lastScanned);
+    alert("Barcode copied: " + lastScanned);
+  }
+});
+
+// Send to Bot tugmasi
+document.getElementById("sendBtn").addEventListener("click", () => {
+  if (lastScanned) {
+    const botUsername = "@aynimo_check_bot"; // 🔹 Bot username yozing
+    const message = encodeURIComponent(lastScanned);
+    window.open(`https://t.me/${botUsername}?start=${message}`, "_blank");
+  }
+});
